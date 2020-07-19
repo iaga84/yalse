0e7f191f391155630018baec4e0d646d9a29d63f
iaga <alessio.iacarelli@wfp.org>
2020-07-19 14:11:08 +0200

    Add more logging

--- yalse-core/yalse_core/library/scan.py
+++ yalse-core/yalse_core/library/scan.py
@@ -13,17 +13,21 @@ from yalse_core.elasticsearch.index import index_document
 def check_anomalies():
     from yalse_core.app import application
     with application.app_context():
+        logging.info("Anomalies detection started..")
         warning_list = []
         for file in db.session.query(File).distinct(File.file_hash):
             if db.session.query(File.id).filter_by(file_hash=file.file_hash, duplicate=False, missing=False, anomaly=False).count() < 1:
                 warning_list.append(file.file_path)
+                logging.warning(f"Anomaly in {file.file_path}")
         if warning_list:
             send_email(f"WARNING! These files are an anomaly: {warning_list}")
+        logging.info("Anomalies detection completed.")
 
 
 def delete_missing_files():
     from yalse_core.app import application
     with application.app_context():
+        logging.info("Missing files deletion started..")
         missing_files = db.session.query(File).filter_by(missing=True).all()
         deleted_files = []
         for missing in missing_files:
@@ -32,16 +36,19 @@ def delete_missing_files():
                     raise Exception
                 db.session.delete(missing)
                 deleted_files.append(missing.file_path)
+                logging.warning(f"MISSING file removed from DB: {missing.file_path}")
             except Exception as e:
                 logging.error(f"Error in removing missing file: {missing.file_path}, {e}")
         db.session.commit()
         if deleted_files:
-            send_email(f"Deleting MISSING records: {deleted_files}")
+            send_email(f"Deleted {len(deleted_files)} MISSING files from the database.")
+        logging.info("Missing files deletion completed. ")
 
 
 def delete_duplicate_files(dry_run):
     from yalse_core.app import application
     with application.app_context():
+        logging.info("Duplicates files deletion started..")
         duplicate_files = db.session.query(File).filter_by(duplicate=True).all()
         deleted_files = []
         for duplicate in duplicate_files:
@@ -85,7 +92,8 @@ def delete_duplicate_files(dry_run):
                 logging.error(f"Error in deleting duplicate file: {duplicate.file_path}, {e}")
         db.session.commit()
         if deleted_files:
-            send_email(f"Deleting {len(deleted_files)} DUPLICATE files: {deleted_files}")
+            send_email(f"DELETED {len(deleted_files)} DUPLICATE files from the system.")
+        logging.info(f"Duplicates files deletion completed: {len(deleted_files)} files deleted.")
 
 
 def index_files():
@@ -105,23 +113,25 @@ def files_scan(dry_run):
                 file.missing = True
         files = []
         count = 0
-        logging.info("Starting filesystem scan..")
+        logging.info("Filesystem scan started..")
         for r, d, f in os.walk(DOCUMENTS_DIR):
             for file in f:
                 count += 1
                 files.append(os.path.join(r, file))
                 if count % 1000 == 0:
                     logging.info(f"Scanned {count} files so far.")
-        logging.info(f"Completed filesystem scan: {len(files)} files found.")
+        logging.info(f"Filesystem scan completed: {len(files)} files found.")
         count = 0
         files_and_hashes = []
-        logging.info("Starting hashing..")
+        logging.info("Hashing started..")
         for file in files:
             count += 1
             files_and_hashes.append((file, SHA256.hash_file(file)))
-            if count % 1000 == 0:
+            if count % 100 == 0:
                 logging.info(f"Hashed {count} files so far.")
-        logging.info(f"Completed hashing.")
+        logging.info(f"Hashing completed.")
+        count = 0
+        logging.info(f"Database processing started..")
         for file_path, file_hash in files_and_hashes:
             count += 1
             path_exists = db.session.query(File.id).filter_by(file_path=file_path).scalar() is not None
@@ -151,14 +161,16 @@ def files_scan(dry_run):
                     file.anomaly = False
             if count % 1000 == 0:
                 logging.info(f"Processed {count} files out of {len(files)} so far.")
-
         db.session.commit()
+        logging.info("Database processing completed.")
 
         check_anomalies()
         delete_missing_files()
         delete_duplicate_files(dry_run)
 
         index_files()
+
+        send_email(f"Tasks sent to Elasticsearch.")
 
 
 def scan_files(body):
